707aab0ee76a16f7103740bad4cf8c7162675823
MakotoAraki <85997616+Makoto-Araki@users.noreply.github.com>
2022-12-15 00:24:57 +0000

    2022年12月15日 - 01

---
+++ model/user.js
@@ -0,0 +1,21 @@
+// Import
+const mongoose = require('mongoose');
+
+// Sub schema
+const subSchema = mongoose.Schema(
+  {
+    description: { type: String },
+    duration: { type: Number },
+    date: { type: Date, default: Date.now }
+  }, 
+  {_id: false}
+);
+
+// Main schema
+const userSchema = new mongoose.Schema({
+  username: { type: String, required: true },
+  logs: [subSchema]
+});
+
+// Model export
+module.exports = mongoose.model('User', userSchema);
