d0fe5f25bcf404b9d24d5c948740aa0568057a52
MakotoAraki <85997616+Makoto-Araki@users.noreply.github.com>
2022-12-15 02:46:12 +0000

    2022年12月15日 - 02

--- index.js
+++ index.js
@@ -1,16 +1,40 @@
-const express = require('express')
-const app = express()
-const cors = require('cors')
+// Import & Config
+const mongoose = require('mongoose');
+const express = require('express');
+const parser = require('body-parser');
+const users = require('./model/users');
+const cors = require('cors');
+const app = express();
 require('dotenv').config()
 
+// Secrets
+const user = process.env['user']
+const pass = process.env['pass']
+const cluster = process.env['cluster']
+const option = process.env['option']
+const database = process.env['database']
+
+// Mongo_URI
+const mongo_URI = `mongodb+srv://${user}:${pass}@${cluster}/${database}?${option}`;
+
+// MongoDB Connect Config
+mongoose.set('strictQuery', false);
+
+// MongoDB Connect
+mongoose.connect(mongo_URI)
+.then(() => console.log('Database connection successed'))
+.catch(err => console.error(err))
+
+// Basic Config
 app.use(cors())
 app.use(express.static('public'))
+
+// GET - [base_url]/
 app.get('/', (req, res) => {
   res.sendFile(__dirname + '/views/index.html')
 });
 
-
-
+// 
 
 
 const listener = app.listen(process.env.PORT || 3000, () => {
--- model/users.js
+++ model/users.js
@@ -18,4 +18,4 @@ const userSchema = new mongoose.Schema({
 });
 
 // Model export
-module.exports = mongoose.model('User', userSchema);
+module.exports = mongoose.model('Users', userSchema);
